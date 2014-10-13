79c7277c49e085d233a0b19ad595f2fd64684bcd
Andrew Scott <andj.scott@gmail.com>
2014-10-13 19:39:08 +0100

    Retrack missing file

---
+++ include/qu3e/q3.h
@@ -0,0 +1,40 @@
+//--------------------------------------------------------------------------------------------------
+/**
+@file	q3.h
+
+@author	Randy Gaul
+@date	10/10/2014
+
+	Copyright (c) 2014 Randy Gaul http://www.randygaul.net
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+	  1. The origin of this software must not be misrepresented; you must not
+	     claim that you wrote the original software. If you use this software
+	     in a product, an acknowledgment in the product documentation would be
+	     appreciated but is not required.
+	  2. Altered source versions must be plainly marked as such, and must not
+	     be misrepresented as being the original software.
+	  3. This notice may not be removed or altered from any source distribution.
+*/
+//--------------------------------------------------------------------------------------------------
+
+#ifndef Q3_H
+#define Q3_H
+
+#include "common/q3Types.h"
+#include "scene/q3Scene.h"
+#include "dynamics/q3Body.h"
+#include "collision/q3Box.h"
+#include "math/q3Vec3.h"
+#include "math/q3Mat3.h"
+#include "math/q3Quaternion.h"
+#include "math/q3Transform.h"
+#include "debug/q3Render.h"
+
+#endif // Q3_H

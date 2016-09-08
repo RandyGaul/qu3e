e1bfbccf95dbea9c7340e8b1e8a860b71d91f155
RandyGaul <r.gaul@digipen.edu>
2016-09-07 18:16:39 -0700

    more comments

--- src/dynamics/q3Contact.h
+++ src/dynamics/q3Contact.h
@@ -53,6 +53,17 @@ inline r32 q3MixFriction( const q3Box* A, const q3Box* B )
 	return std::sqrt( A->friction * B->friction );
 }
 
+// in stands for "incoming"
+// out stands for "outgoing"
+// I stands for "incident"
+// R stands for "reference"
+// See D. Gregorius GDC 2015 on creating contacts for more details
+// Each feature pair is used to cache solutions from one physics tick to another. This is
+// called warmstarting, and lets boxes stack and stay stable. Feature pairs identify points
+// of contact over multiple physics ticks. Each feature pair is the junction of an incoming
+// feature and an outgoing feature, usually a result of clipping routines. The exact info
+// stored in the feature pair can be arbitrary as long as the result is a unique ID for a
+// given intersecting configuration.
 union q3FeaturePair
 {
 	struct
